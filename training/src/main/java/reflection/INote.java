b70bca5ec3ae5027267b02fba3da0bf1d02c1574
MykolaKosharnyi <nikolay.kosharniy@gmail.com>
2016-11-12 19:02:26 +0200

    Homework with reflection.

---
+++ training/src/main/java/reflection/INote.java
@@ -0,0 +1,16 @@
+package reflection;
+
+public interface INote {
+
+    String getFirstName();
+    void setFirstName(String firstName);
+    String getLastName();
+    void setLastName(String lastName);
+    String getMiddleName();
+    void setMiddleName(String middleName);
+    int getPhoneNumber();
+    void setPhoneNumber(int phoneNumber);
+    Date getBirthday();
+    void setBirthday(Date birthday);
+
+}
